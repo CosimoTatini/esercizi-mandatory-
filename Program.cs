3a3e43363c6fdda74ad4df6572ba46a26d7964d8
CosimoTatini <151754636+CosimoTatini@users.noreply.github.com>
2023-12-03 19:44:12 +0100

    esercizio password segreta

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Password segreta
+string passwordSegreta = "tucano";
+
+do // ciclo do while per richiedere la password
+{
+    Console.Write("Inserisci la password: ");
+    string passwordUtente = Console.ReadLine();
+
+    // Verifica se la password è corretta
+    if (passwordUtente == passwordSegreta)
+    {
+        Console.WriteLine("Complimenti la password è corretta!");
+        return;  // Esci dal ciclo do-while se la password è corretta
+    }
+    else
+    {
+        Console.WriteLine("Mi dispiace la password è sbagliata! Riprova...");
+    }
+
+} while (true);
+    
