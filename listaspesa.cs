9e8c91478f0b8e9f43a18bd5bf9abbbb31fa6d53
CosimoTatini <151754636+CosimoTatini@users.noreply.github.com>
2023-12-11 13:00:37 +0100

    Update listaspesa.cs

--- listaspesa.cs
+++ listaspesa.cs
@@ -1,38 +1,62 @@
-﻿
+﻿// Definire lista della spesa
+List<string> lista = new List<string>();
+
+Console.WriteLine("Inserisci elementi nella lista(digita 'fine' per terminare l'inserimento)");
+string elemento;
+while (true)
 {
-    {
-        Console.WriteLine("Inserisci la lunghezza del lato del triangolo:");
-        
-        // Leggi la lunghezza inserita dall'utente
-        if (int.TryParse(Console.ReadLine(), out int lunghezza))
-        {
-            // Disegna il triangolo
-            DisegnaTriangolo(lunghezza);
-        }
-        else
-        {
-            Console.WriteLine("Inserisci un valore valido.");
-        }
+    Console.Write("Elemento: ");
+    elemento = Console.ReadLine();
+    if (elemento.ToLower() == "fine")
+    { 
+     break;
     }
+    lista.Add(elemento);
+}
+// visualiazzione lista
+Console.WriteLine("\n Lista della spesa:");
+foreach (var oggetto in lista)
+{
+    Console.WriteLine($"-{oggetto}");
+}
+// Calcolo totale
+int stimacosto = 3; // valore arbitrario
+int totalespesa= CalcoloTotaleSpesa(lista,stimacosto);
+Console.WriteLine($"\nTotale della Spesa: {totalespesa}, ");
 
-    static void DisegnaTriangolo(int lunghezza)
+//Ricerca elementi
+Console.Write("\nInserisci l'elemento da cercare: ");
+string searchelement= Console.ReadLine();
+searchelementinlist(lista,searchelement);
+
+static int CalcoloTotaleSpesa(List<string> lista, int stimacosto)
+{
+    int totalespesa=0;
+    foreach(var elemento in lista)
     {
-        for (int i = 1; i <= lunghezza; i++)
-        {
-            // Stampa gli spazi bianchi a sinistra del triangolo
-            for (int j = 0; j < lunghezza - i; j++)
-            {
-                Console.Write(" ");
-            }
-
-            // Stampa gli asterischi del triangolo
-            for (int j = 0; j < 2 * i - 1; j++)
-            {
-                Console.Write("*");
-            }
-
-            // Vai a una nuova linea dopo aver stampato una riga del triangolo
-            Console.WriteLine();
-        }
+        totalespesa += stimacosto;
+    }
+    return totalespesa;
+}
+static void searchelementinlist(List<string> lista, string searchelement)
+{
+    bool findelement=false;
+    double findelementcost = 0;
+    foreach(var elemento in lista)
+    { 
+     if (elemento.Equals(searchelement, StringComparison.OrdinalIgnoreCase)) 
+        { 
+        findelement = true;
+        findelementcost = 3; // sostiuisci con costo effettivo dell'elemento
+            break;
+       }
+    }
+    if (findelement)
+    {
+        Console.WriteLine($"L'elemento '{searchelement}' è presente nella lista e costa {findelementcost}");
+    }
+    else
+    {
+        Console.WriteLine($"L'elemento '{searchelement}'non è presente nella lista.");
     }
 }
