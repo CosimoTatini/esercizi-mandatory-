2f484537d57dd4707858cf0ef721483294220f70
Cosimo Tatini <cosimmo.tatini@gmail.com>
2023-12-13 16:50:29 +0100

    libro

---
+++ libro 2/libro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Libro
+{
+    // Campi della classe Libro
+    public string Titolo;
+    public string Autore;
+    public int AnnoPubblicazione;
+
+    // Costruttore per inizializzare i campi
+    public Libro(string titolo, string autore, int annoPubblicazione)
+    {
+        Titolo = titolo;
+        Autore = autore;
+        AnnoPubblicazione = annoPubblicazione;
+    }
+
+    // Metodo per visualizzare le informazioni del libro
+    public void VisualizzaInfo()
+    {
+        Console.WriteLine($"Titolo: {Titolo}");
+        Console.WriteLine($"Autore: {Autore}");
+        Console.WriteLine($"Anno di Pubblicazione: {AnnoPubblicazione}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //utilizzo della classe Libro
+        Libro mioLibro = new Libro("L'arte di Insultare", "Arthur Schopenauer", 1999);
+        mioLibro.VisualizzaInfo();
+
+
+
+    }
+}
